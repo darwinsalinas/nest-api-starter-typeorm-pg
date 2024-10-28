cb64d5194777d789dc4dc92b29b70df7094ad00f
Darwin Salinas <salinash2000@gmail.com>
2024-10-28 15:18:37 -0600

    Refactor UserRoleGuard to validate user roles against valid roles from database

--- src/auth/guards/user-role.guard.ts
+++ src/auth/guards/user-role.guard.ts
@@ -3,32 +3,54 @@ import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
 import { User } from 'src/auth/entities/user.entity';
 import { META_ROLES } from '../decorators';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UserRoleGuard implements CanActivate {
 
   constructor(
-    private readonly reflector: Reflector
+    private readonly reflector: Reflector,
+
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
   ) { }
 
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
 
-    const validRoles = this.reflector.get(META_ROLES, context.getHandler());
+    const validRoles: string[] = this.reflector.get(META_ROLES, context.getHandler());
 
     if (!validRoles) return true;
     if (validRoles.length === 0) return true;
 
-    const request = context.switchToHttp().getRequest()
+    return this.validateRequest(context, validRoles)
+  }
+
+  async validateRequest(execContext: ExecutionContext, validRoles: string[]): Promise<boolean> {
+    const request = execContext.switchToHttp().getRequest();
     const user = request.user as User;
 
     if (!user) {
       throw new BadRequestException('User not found in request');
     }
 
-    for (const role of user.roles) {
-      if (validRoles.includes(role)) {
+    const dbUser = await this.userRepository.findOne({
+      where: {
+        email: user.email,
+        isActive: true
+      },
+      select: ['id', 'email', 'password', 'roles', 'directPermissions',],
+      relations: ['roles.permissions', 'directPermissions']
+    });
+
+    if (!dbUser) {
+      throw new BadRequestException('User is not active or does not exist');
+    }
+
+    for (const role of dbUser.roles) {
+      if (validRoles.includes(role.role)) {
         return true;
       }
     }
