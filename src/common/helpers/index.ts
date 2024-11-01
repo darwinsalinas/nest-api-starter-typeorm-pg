c9ceea55f34a1fc2d519e4e970c5240f29a6412f
Darwin Salinas <salinash2000@gmail.com>
2024-11-01 10:27:11 -0600

    Fix file name typo in exception handler

--- src/common/helpers/index.ts
+++ src/common/helpers/index.ts
@@ -2,4 +2,4 @@ export { validatePassword } from './validate-password.helper';
 export { hashPassword, hashPasswordSync } from './hash-password.helper';
 export { generateSlug } from './generate-slug.helper';
 export { isValidUUID } from './is-valid-uuid.helper';
-export { handleError } from './exeption-handler.helper';
+export { handleError } from './exception-handler.helper';
