335bda4bdae788ae9afb1580c4a61b4b73009cec
Darwin Salinas <salinash2000@gmail.com>
2024-11-01 17:15:11 -0600

    Add migration to create companies table

---
+++ src/migrations/1730500699168-create_table_companies.ts
@@ -0,0 +1,58 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateTableCompanies1730500699168 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'companies',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            length: '150',
+          },
+          {
+            name: 'ruc',
+            type: 'varchar',
+            length: '30',
+            isNullable: true,
+          },
+          {
+            name: 'phone',
+            type: 'varchar',
+            length: '20',
+            isNullable: true,
+          },
+          {
+            name: 'address',
+            type: 'varchar',
+            length: '200',
+            isNullable: true,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('companies');
+  }
+}
